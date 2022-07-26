cf5a8de40aa909181a13c65ef986cf5a7206ede5
Scott Durrett <icodeintx@gmail.com>
2022-07-25 19:54:26 -0500

    Updated scripts

--- search.ps1
+++ search.ps1
@@ -1,5 +1,12 @@
 
 $SearchString = $args[0]
 
-#Get-ChildItem -Path .\ -Filter $SearchString -Recurse -File| Sort-Object Length -Descending | ForEach-Object {$_.BaseName}
+#if the search pattern is empty do not continue
+#CLI then exit the script
+if($SearchString -eq "" -or $args.Count -eq 0)
+{
+    Write-Host "Search pattern must be provided."
+    Return
+}
+
 Get-ChildItem -Path .\ -Filter $SearchString -Recurse -File| Sort-Object Length -Descending | ForEach-Object {$_.FullName}
--- searchin.ps1
+++ searchin.ps1
@@ -1,4 +1,12 @@
 
 $SearchString = $args[0]
 
+#if the search pattern is empty do not continue
+#CLI then exit the script
+if($SearchString -eq "" -or $args.Count -eq 0)
+{
+    Write-Host "Search pattern must be provided."
+    Return
+}
+
 Get-ChildItem -Recurse | Select-String $SearchString -List | Select-Object Path, LineNumber, Matches, Line
