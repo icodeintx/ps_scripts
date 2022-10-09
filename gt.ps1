56644bd586ef688b57f1ca7afd5d3791bec201d3
Scott Durrett <icodeintx@gmail.com>
2022-10-09 08:14:24 -0500

    added gt.xxx

---
+++ gt.ps1
@@ -0,0 +1,58 @@
+
+#read the json data
+$json = (Get-Content "c:\\src\\path\\gt.json" -Raw) | ConvertFrom-Json | Sort-Object title
+
+
+
+#initialize the dir variable
+$dir = ""
+
+#check the value of the argument (first and only)
+if ($args[0].Count -eq 0)
+{   
+    #IF NO ARGUMENTS JUST WRITE THE JSON FILE OUTPUT
+   #Clear-Host 
+    Write-Output $json;
+    #Write-Output "test        stest";
+    #Write-Output "Home        $Env:HomeDrive$Env:HomePath";
+}
+elseif($args[0] -eq "Home")
+{
+    #If the target is "Home" then go to the computer home path
+    cd -Path $Env:HomePath
+}
+else
+{
+
+    #WRITE BLANK LINE
+    Write-Output ""
+    
+    #Loop through all of the elements in the json file
+    foreach ($i in $json.GetEnumerator()) { 
+
+        #If the command matches what is in the json data then change directory to that path
+        if ($i.command -eq $args[0])
+         {
+            #set the dir variable
+            $dir = $args[0]
+
+            Write-Output "Changing directories... " $i.directory
+            
+            #do the change directory
+            cd -Path $i.directory
+            break
+
+        }
+    }
+
+
+    #if we have looped all the elements and no matches then give not listed notice
+    if ($dir -eq "")
+    {
+        Write-Output "Directory not listed in JSON file."
+    }
+
+}
+
+
+Write-Output ""
