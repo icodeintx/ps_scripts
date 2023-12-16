e0734b0aed499d8e7f97801209f9841661d974a5
Scott Durrett <icodeintx@gmail.com>
2023-12-16 06:58:57 -0600

    modified gitcleanup script

--- gitcleanup.ps1
+++ gitcleanup.ps1
@@ -1,8 +1,22 @@
+param (
+   [Parameter(Mandatory=$true)]
+   [string]$BranchToKeep
+)
 
-Write-Host "Removing ALL Git branches but Master"
+# this block is pointless and will never be hit due to mandatory parameter
+if ([string]::IsNullOrEmpty($BranchToKeep)) {
+   Write-Host "You MUST provide a branch name to keep"
+   return
+}
 
-git checkout master
+#ask the user if they would like push the code to the cloud repository
+$AreYouSure = Read-Host "Are you SURE you want to run this script? Note the branch you are keeping.  Is it 'main' or 'master' << You MUST type 'yes' to continue >>" 
 
-git branch -D  @(git branch | select-string -NotMatch "master" | Foreach {$_.Line.Trim()})
+if($AreYouSure -eq "yes")
+{
+   Write-Host "Removing ALL Git branches except $BranchToKeep"
 
-
+   git checkout $BranchToKeep
+   
+   git branch -D  @(git branch | select-string -NotMatch $BranchToKeep | Foreach {$_.Line.Trim()})
+}
